e78d305a74b26c0130ef5de1683a2915452cf393
Joseph <jjempy@gmail.com>
2026-04-05 16:15:46 -0400

    SEO meta tags, formula section redesign, sitemap, robots.txt

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { copyFileSync } from 'fs'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    {
+      name: 'copy-redirects',
+      closeBundle() {
+        copyFileSync('public/_redirects', 'dist/_redirects')
+      },
+    },
+  ],
   build: {
     rollupOptions: {
       output: {
